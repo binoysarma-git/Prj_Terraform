96ecce5e0a93e969acb549880ee6a4543140e5ae
binoysarma-git <binoy_sarma@hotmail.com>
2025-01-27 00:08:06 +0530

    c

--- main.tf
+++ main.tf
@@ -13,18 +13,25 @@ provider "azurerm" {
 }
 
 # Create a resource group
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
+resource "azurerm_resource_group" "example_git" {
+  name     = "example-resources-git"
   location = "East US"
 }
 #create a virtual network
-resource "azurerm_virtual_network" "example_vn"{
-  name="example_vn"
-  resource_group_name=azurerm_resource_group.example.name
-  location=azurerm_resource_group.example.location
+resource "azurerm_virtual_network" "example_git_vn"{
+  name="example_git_vn"
+  resource_group_name=azurerm_resource_group.example_git.name
+  location=azurerm_resource_group.example_git.location
   address_space=["10.123.0.0/16"]
   tags={
     environment="dev"
   }
 }
 
+#Create a Subnet 
+resource "azurerm_subnet" "example_git_subnet"{
+  name="example_git_subnet"
+  resource_group_name=azurerm_resource_group.example_git.name
+  virtual_network_name=azurerm_virtual_network.example_git_vn.name
+  address_prefixes=["10.123.1.0/24"]
+}
