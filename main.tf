74ef97cde34af0116b6c988cdb35acc4d50cf83e
binoysarma-git <binoy_sarma@hotmail.com>
2025-01-26 23:16:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,22 +1,30 @@
+# Azure Provider source and version being used
 terraform {
   required_providers {
-    azurerm={
-        version = "3.115.0"
-        source="hashicorp/azurerm"
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.112.0"
     }
   }
 }
-  provider "azurerm"{
-    features{
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
 
-    }
+# Create a resource group
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "East US"
+}
+#create a virtual network
+resource "azurerm_virtual_network" "example_vn"{
+  name="example_vn"
+  resource_group_name=azurerm_resource_group.example.name
+  location=azurerm_resource_group.example.location
+  address_space=["10.123.0.0/16"]
+  tags={
+    environment="dev"
   }
- 
- resource "azurerm_resource_group" "terrform_rg_day1"{
-    name ="mytf-rg-day1"
-    location= "centralindia"
-    tags={
+}
 
-        "terraform"="terraform"
-    }
- }
